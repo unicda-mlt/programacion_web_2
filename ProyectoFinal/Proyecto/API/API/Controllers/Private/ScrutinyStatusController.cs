e0b29b37ca7673eaf3371779b051f44b36b14fc3
MaximoLiberata <maximoliberata0@gmail.com>
2026-03-16 14:46:51 -0400

    feat: add ScrutinyStatusController to manage scrutiny status retrieval

---
+++ ProyectoFinal/Proyecto/API/API/Controllers/Private/ScrutinyStatusController.cs
@@ -0,0 +1,37 @@
+﻿using Business.Authentication;
+using Domain.API;
+using Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace API.Controllers.Private
+{
+    [AuthorizeUserRoleAttribute(EUserRole.ADMIN)]
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ScrutinyStatusController() : ControllerBase
+    {
+        [HttpGet()]
+        [ProducesResponseType<EnumResponse<EScrutinyStatus>>(StatusCodes.Status200OK)]
+        [SwaggerOperation(
+            Summary = "Obtener estados de escrutinios.",
+            Description = "Devuelve todos los estados posibles de los escrutinios."
+        )]
+        public IActionResult Get()
+        {
+            var data = EScrutinyStatusExtensions.GetList().Select(x => new EnumResponse<EScrutinyStatus>.DataResponse
+            {
+                Value = (EScrutinyStatus)EScrutinyStatusExtensions.FromValue(x.Value),
+                Name = x.Name,
+            }).ToArray();
+
+            return Ok(new EnumResponse<EScrutinyStatus>
+            {
+                Ok = true,
+                Data = data
+            });
+        }
+    }
+}
